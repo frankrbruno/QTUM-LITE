19a555166043d8ee4d7cbfcf9492e7e629a3a4d0
frankrbruno <frankrbruno@yahoo.com>
2018-01-14 12:12:54 -0500

    Update gitian-build.sh

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -17,7 +17,7 @@ osx=true
 SIGNER=
 VERSION=
 commit=false
-url=https://github.com/qtum-liteproject/qtum-lite
+url=https://github.com/qtumliteproject/qtumlite
 proc=2
 mem=2000
 lxc=true
@@ -31,7 +31,7 @@ commitFiles=true
 read -d '' usage <<- EOF
 Usage: $scriptName [-c|u|v|b|s|B|o|h|j|m|] signer version
 
-Run this script from the directory containing the qtum-lite, gitian-builder, gitian.sigs, and qtum-lite-detached-sigs.
+Run this script from the directory containing the qtumlite, gitian-builder, gitian.sigs, and qtumlite-detached-sigs.
 
 Arguments:
 signer          GPG signer to sign each build assert file
@@ -39,7 +39,7 @@ version		Version number, commit, or branch to build. If building a commit or bra
 
 Options:
 -c|--commit	Indicate that the version argument is for a commit or branch
--u|--url	Specify the URL of the repository. Default is https://github.com/qtum-liteproject/qtum-lite
+-u|--url	Specify the URL of the repository. Default is https://github.com/qtumliteproject/qtumlite
 -v|--verify 	Verify the gitian build
 -b|--build	Do a gitian build
 -s|--sign	Make signed binaries for Windows and Mac OSX
@@ -232,8 +232,8 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/qtum-liteproject/gitian.sigs.git
-    git clone https://github.com/qtum-liteproject/qtum-lite-detached-sigs.git
+    git clone https://github.com/qtumliteproject/gitian.sigs.git
+    git clone https://github.com/qtumliteproject/qtumlite-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
@@ -247,7 +247,7 @@ then
 fi
 
 # Set up build
-pushd ./qtum-lite
+pushd ./qtumlite
 git fetch
 git checkout ${COMMIT}
 popd
@@ -256,7 +256,7 @@ popd
 if [[ $build = true ]]
 then
 	# Make output folder
-	mkdir -p ./qtum-lite-binaries/${VERSION}
+	mkdir -p ./qtumlite-binaries/${VERSION}
 	
 	# Build Dependencies
 	echo ""
@@ -266,7 +266,7 @@ then
 	mkdir -p inputs
 	wget -N -P inputs $osslPatchUrl
 	wget -N -P inputs $osslTarUrl
-	make -C ../qtum-lite/depends download SOURCES_PATH=`pwd`/cache/common
+	make -C ../qtumlite/depends download SOURCES_PATH=`pwd`/cache/common
 
 	# Linux
 	if [[ $linux = true ]]
@@ -274,9 +274,9 @@ then
             echo ""
 	    echo "Compiling ${VERSION} Linux"
 	    echo ""
-	    ./bin/gbuild -j ${proc} -m ${mem} --commit qtum-lite=${COMMIT} --url qtum-lite=${url} ../qtum-lite/contrib/gitian-descriptors/gitian-linux.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-linux --destination ../gitian.sigs/ ../qtum-lite/contrib/gitian-descriptors/gitian-linux.yml
-	    mv build/out/qtum-lite-*.tar.gz build/out/src/qtum-lite-*.tar.gz ../qtum-lite-binaries/${VERSION}
+	    ./bin/gbuild -j ${proc} -m ${mem} --commit qtumlite=${COMMIT} --url qtumlite=${url} ../qtumlite/contrib/gitian-descriptors/gitian-linux.yml
+	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-linux --destination ../gitian.sigs/ ../qtumlite/contrib/gitian-descriptors/gitian-linux.yml
+	    mv build/out/qtumlite-*.tar.gz build/out/src/qtumlite-*.tar.gz ../qtumlite-binaries/${VERSION}
 	fi
 	# Windows
 	if [[ $windows = true ]]
@@ -284,10 +284,10 @@ then
 	    echo ""
 	    echo "Compiling ${VERSION} Windows"
 	    echo ""
-	    ./bin/gbuild -j ${proc} -m ${mem} --commit qtum-lite=${COMMIT} --url qtum-lite=${url} ../qtum-lite/contrib/gitian-descriptors/gitian-win.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-win-unsigned --destination ../gitian.sigs/ ../qtum-lite/contrib/gitian-descriptors/gitian-win.yml
-	    mv build/out/qtum-lite-*-win-unsigned.tar.gz inputs/qtum-win-unsigned.tar.gz
-	    mv build/out/qtum-lite-*.zip build/out/qtum-lite-*.exe ../qtum-lite-binaries/${VERSION}
+	    ./bin/gbuild -j ${proc} -m ${mem} --commit qtumlite=${COMMIT} --url qtumlite=${url} ../qtumlite/contrib/gitian-descriptors/gitian-win.yml
+	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-win-unsigned --destination ../gitian.sigs/ ../qtumlite/contrib/gitian-descriptors/gitian-win.yml
+	    mv build/out/qtumlite-*-win-unsigned.tar.gz inputs/qtumlite-win-unsigned.tar.gz
+	    mv build/out/qtumlite-*.zip build/out/qtumlite-*.exe ../qtumlite-binaries/${VERSION}
 	fi
 	# Mac OSX
 	if [[ $osx = true ]]
@@ -295,10 +295,10 @@ then
 	    echo ""
 	    echo "Compiling ${VERSION} Mac OSX"
 	    echo ""
-	    ./bin/gbuild -j ${proc} -m ${mem} --commit qtum-lite=${COMMIT} --url qtum-lite=${url} ../qtum-lite/contrib/gitian-descriptors/gitian-osx.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-osx-unsigned --destination ../gitian.sigs/ ../qtum-lite/contrib/gitian-descriptors/gitian-osx.yml
-	    mv build/out/qtum-lite-*-osx-unsigned.tar.gz inputs/qtum-lite-osx-unsigned.tar.gz
-	    mv build/out/qtum-lite-*.tar.gz build/out/qtum-lite-*.dmg ../qtum-lite-binaries/${VERSION}
+	    ./bin/gbuild -j ${proc} -m ${mem} --commit qtumlite=${COMMIT} --url qtumlite=${url} ../qtumlite/contrib/gitian-descriptors/gitian-osx.yml
+	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-osx-unsigned --destination ../gitian.sigs/ ../qtumlite/contrib/gitian-descriptors/gitian-osx.yml
+	    mv build/out/qtumlite-*-osx-unsigned.tar.gz inputs/qtumlite-osx-unsigned.tar.gz
+	    mv build/out/qtumlite-*.tar.gz build/out/qtumlite-*.dmg ../qtumlite-binaries/${VERSION}
 	fi
 	popd
 
@@ -325,27 +325,27 @@ then
 	echo ""
 	echo "Verifying v${VERSION} Linux"
 	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-linux ../qtum-lite/contrib/gitian-descriptors/gitian-linux.yml
+	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-linux ../qtumlite/contrib/gitian-descriptors/gitian-linux.yml
 	# Windows
 	echo ""
 	echo "Verifying v${VERSION} Windows"
 	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-win-unsigned ../qtum-lite/contrib/gitian-descriptors/gitian-win.yml
+	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-win-unsigned ../qtumlite/contrib/gitian-descriptors/gitian-win.yml
 	# Mac OSX	
 	echo ""
 	echo "Verifying v${VERSION} Mac OSX"
 	echo ""	
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-unsigned ../qtum-lite/contrib/gitian-descriptors/gitian-osx.yml
+	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-unsigned ../qtumlite/contrib/gitian-descriptors/gitian-osx.yml
 	# Signed Windows
 	echo ""
 	echo "Verifying v${VERSION} Signed Windows"
 	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-signed ../qtum-lite/contrib/gitian-descriptors/gitian-osx-signer.yml
+	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-signed ../qtumlite/contrib/gitian-descriptors/gitian-osx-signer.yml
 	# Signed Mac OSX
 	echo ""
 	echo "Verifying v${VERSION} Signed Mac OSX"
 	echo ""
-	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-signed ../qtum-lite/contrib/gitian-descriptors/gitian-osx-signer.yml	
+	./bin/gverify -v -d ../gitian.sigs/ -r ${VERSION}-osx-signed ../qtumlite/contrib/gitian-descriptors/gitian-osx-signer.yml	
 	popd
 fi
 
@@ -360,10 +360,10 @@ then
 	    echo ""
 	    echo "Signing ${VERSION} Windows"
 	    echo ""
-	    ./bin/gbuild -i --commit signature=${COMMIT} ../qtum-lite/contrib/gitian-descriptors/gitian-win-signer.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-win-signed --destination ../gitian.sigs/ ../qtum-lite/contrib/gitian-descriptors/gitian-win-signer.yml
-	    mv build/out/qtum-lite-*win64-setup.exe ../qtum-lite-binaries/${VERSION}
-	    mv build/out/qtum-lite-*win32-setup.exe ../qtum-lite-binaries/${VERSION}
+	    ./bin/gbuild -i --commit signature=${COMMIT} ../qtumlite/contrib/gitian-descriptors/gitian-win-signer.yml
+	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-win-signed --destination ../gitian.sigs/ ../qtumlite/contrib/gitian-descriptors/gitian-win-signer.yml
+	    mv build/out/qtumlite-*win64-setup.exe ../qtumlite-binaries/${VERSION}
+	    mv build/out/qtumlite-*win32-setup.exe ../qtumlite-binaries/${VERSION}
 	fi
 	# Sign Mac OSX
 	if [[ $osx = true ]]
@@ -371,9 +371,9 @@ then
 	    echo ""
 	    echo "Signing ${VERSION} Mac OSX"
 	    echo ""
-	    ./bin/gbuild -i --commit signature=${COMMIT} ../qtum-lite/contrib/gitian-descriptors/gitian-osx-signer.yml
-	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-osx-signed --destination ../gitian.sigs/ ../qtum-lite/contrib/gitian-descriptors/gitian-osx-signer.yml
-	    mv build/out/qtum-lite-osx-signed.dmg ../qtum-lite-binaries/${VERSION}/qtum-lite-${VERSION}-osx.dmg
+	    ./bin/gbuild -i --commit signature=${COMMIT} ../qtumlite/contrib/gitian-descriptors/gitian-osx-signer.yml
+	    ./bin/gsign -p $signProg --signer $SIGNER --release ${VERSION}-osx-signed --destination ../gitian.sigs/ ../qtumlite/contrib/gitian-descriptors/gitian-osx-signer.yml
+	    mv build/out/qtumlite-osx-signed.dmg ../qtumlite-binaries/${VERSION}/qtumlite-${VERSION}-osx.dmg
 	fi
 	popd
 
